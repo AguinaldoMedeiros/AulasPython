fd6cee0366f40439d60a15f2a4578b4f55dee5c7
AguinaldoMedeiros <aguinaldo.medeiros.coder@gmail.com>
2024-07-15 00:26:53 -0300

    Concluindo a aula07

--- aula07.py
+++ aula07.py
@@ -5,3 +5,6 @@
 # atribuir um valor a um nome (variável).
 # Uso: nome_variável = expressão
 
+nome_completo =  'Aguinaldo Medeiros'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
