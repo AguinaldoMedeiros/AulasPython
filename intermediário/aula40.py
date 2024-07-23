2cc41b8ae27f1e1ddfc030b3ff276fd23f129b62
AguinaldoMedeiros <aguinaldo.medeiros.coder@gmail.com>
2024-07-22 23:06:05 -0300

    Adicionando aulas 41 a 44

--- intermediário/aula40.py
+++ intermediário/aula40.py
@@ -2,8 +2,6 @@
 # Aumente os preços dos produtos a seguir em 10%
 # Gera novos_produtos por deep copy (cópia profunda)
 import copy
-from intermediário.dados import produtos_m
-
 
 produtos = [
     {'nome': 'Produto 5', 'preco': 10.00},
@@ -15,22 +13,29 @@ produtos = [
 
 novos_produtos = copy.deepcopy(produtos)
 
-novos_produtos = [{**produto,'preco': produto['preco']*1.10} for produto in novos_produtos]
+novos_produtos = [{**produto,'preco': round(produto['preco']*1.10)} for produto in novos_produtos]
 
 # Ordene os produtos por nome descrente (do maior para o menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
 produtos_ordenados_por_nome = copy.deepcopy(produtos)
 
-produtos_ordenados_por_nome = sorted(produtos_ordenados_por_nome, key=lambda item: item['nome'], reverse=True)
+produtos_ordenados_por_nome = sorted(
+    produtos_ordenados_por_nome, 
+    key=lambda item: item['nome'], 
+    reverse=True
+)
 
 
 # Ordene os produtos por preco crescente (do menor para o maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
 
 produtos_ordenados_por_preco = copy.deepcopy(produtos)
-produtos_ordenados_por_preco = sorted(produtos_ordenados_por_preco, key=lambda item:
-                                      item['preco'], reverse=True)
+produtos_ordenados_por_preco = sorted(
+    produtos_ordenados_por_preco, 
+    key=lambda item: item['preco'], 
+    reverse=True
+)
 
 print(*produtos, sep='\n')
 print()
