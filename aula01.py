852e4b0f4891b3806fc24c2dd3a37e65b80463ca
AguinaldoMedeiros <aguinaldo.medeiros.coder@gmail.com>
2024-08-05 15:31:18 -0300

    Iniciando os módulos em Python

---
+++ aula01.py
@@ -0,0 +1,19 @@
+# Criando datas com módulo datetime
+# datetime(ano, mês, dia)
+# datetime(ano, mês, dia, horas, minutos, segundos)
+# datetime.strptime('DATA', 'FORMATO')
+# datetime.now()
+# https://pt.wikipedia.org/wiki/Era_Unix
+# datetime.fromtimestamp(Unix Timestamp)
+# https://docs.python.org/3/library/datetime.html
+# Para timezones
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# Instalando o pytz
+# pip install pytz types-pytz
+
+
+from datetime import datetime
+
+data = datetime(2024, 8, 5, 14, 42, 3, 0)
+
+print(data)
